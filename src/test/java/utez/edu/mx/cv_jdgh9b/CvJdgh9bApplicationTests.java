6823c0a274bd6bd9a5bc8378dbf1829c43350dc9
Juan Diego <20223tn127@utez.edu.mx>
2025-05-27 15:51:29 -0600

    [FIRST] Proyecto Inicial

---
+++ src/test/java/utez/edu/mx/cv_jdgh9b/CvJdgh9bApplicationTests.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.cv_jdgh9b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvJdgh9bApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
